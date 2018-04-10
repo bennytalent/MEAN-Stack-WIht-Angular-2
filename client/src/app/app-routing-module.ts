c0c5f8c2094ecde02612d4f9e828534c2f88b43a
Kagerer <kagerer.b@idowapro.de>
2018-04-10 11:27:47 +0200

    created authGuards

--- client/src/app/app-routing-module.ts
+++ client/src/app/app-routing-module.ts
@@ -5,6 +5,8 @@ import { DashboardComponent } from "./components/dashboard/dashboard.component";
 import { RegisterComponent } from './components/register/register.component';
 import { LoginComponent} from "./components/login/login.component";
 import { ProfileComponent } from "./components/profile/profile.component";
+import { AuthGuard } from "./guards/auth.guard";
+import { NoAuthGuard } from "./guards/noAuth.guard";
 
 const appRoutes: Routes = [
   {
@@ -13,19 +15,23 @@ const appRoutes: Routes = [
   },
   {
     path: 'dashboard',
-    component: DashboardComponent
+    component: DashboardComponent,
+    canActivate: [AuthGuard]
   },
   {
     path: 'register',
-    component: RegisterComponent
+    component: RegisterComponent,
+    canActivate: [NoAuthGuard]
   },
   {
     path: 'login',
-    component: LoginComponent
+    component: LoginComponent,
+    canActivate: [NoAuthGuard]
   },
   {
     path: 'profile',
-    component: ProfileComponent
+    component: ProfileComponent,
+    canActivate: [AuthGuard]
   },
   { path: '**', component: HomeComponent }
 ];
--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -12,7 +12,8 @@ import { AuthService } from './services/auth.service';
 import { LoginComponent } from './components/login/login.component';
 import { ProfileComponent } from './components/profile/profile.component'
 import { FlashMessagesModule} from "angular2-flash-messages";
-
+import { AuthGuard } from "./guards/auth.guard";
+import { NoAuthGuard } from "./guards/noAuth.guard";
 
 @NgModule({
   declarations: [
@@ -32,7 +33,7 @@ import { FlashMessagesModule} from "angular2-flash-messages";
     FlashMessagesModule.forRoot()
 
   ],
-  providers: [AuthService],
+  providers: [AuthService, AuthGuard, NoAuthGuard],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
