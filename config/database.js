3df7f914d0d74f517cde94d6b8e8a505222b668a
bennytalent <ben.kagerer@gmx.de>
2018-04-15 22:59:56 +0200

    deployment

--- config/database.js
+++ config/database.js
@@ -1,8 +1,9 @@
 const crypto = require('crypto').randomBytes(256).toString('hex');
 
 module.exports = {
-    uri: 'mongodb://localhost:27017/mean-angular-2',
+    //uri: 'mongodb://localhost:27017/mean-angular-2',
+    uri: 'mongodb://benny:benny@ds133558.mlab.com:33558/angular-2-app', //Production
     secret: crypto,
-    db: 'mean-angular-2'
-
+    //db: 'mean-angular-2'
+    db: 'angular-2-app' //Production
 };
